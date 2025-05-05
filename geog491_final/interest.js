11bad981f1c0f3a9d78de405fba792990d24cc94
Campbellworsley3 <campbellworsley@gmail.com>
2025-05-04 21:21:49 -0400

    geog491final

---
+++ geog491_final/interest.js
@@ -0,0 +1,142 @@
+var interest = [
+  {
+    "Year": 2018,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 2.76
+  },
+  {
+    "Year": 2018,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 2.91
+  },
+  {
+    "Year": 2018,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 2.93
+  },
+  {
+    "Year": 2018,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 3
+  },
+  {
+    "Year": 2019,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 2.57
+  },
+  {
+    "Year": 2019,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 2.33
+  },
+  {
+    "Year": 2019,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 1.79
+  },
+  {
+    "Year": 2019,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 1.8
+  },
+  {
+    "Year": 2020,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 1.37
+  },
+  {
+    "Year": 2020,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 0.69
+  },
+  {
+    "Year": 2020,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 0.65
+  },
+  {
+    "Year": 2020,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 0.85
+  },
+  {
+    "Year": 2021,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 1.34
+  },
+  {
+    "Year": 2021,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 1.58
+  },
+  {
+    "Year": 2021,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 1.32
+  },
+  {
+    "Year": 2021,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 1.53
+  },
+  {
+    "Year": 2022,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 1.97
+  },
+  {
+    "Year": 2022,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 2.93
+  },
+  {
+    "Year": 2022,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 3.1
+  },
+  {
+    "Year": 2022,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 3.83
+  },
+  {
+    "Year": 2023,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 3.52
+  },
+  {
+    "Year": 2023,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 3.75
+  },
+  {
+    "Year": 2023,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 4.1
+  },
+  {
+    "Year": 2023,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 4.25
+  },
+  {
+    "Year": 2024,
+    "Quarter": "Q1",
+    "Average_10Y_Treasury_Yield": 4.3
+  },
+  {
+    "Year": 2024,
+    "Quarter": "Q2",
+    "Average_10Y_Treasury_Yield": 4.15
+  },
+  {
+    "Year": 2024,
+    "Quarter": "Q3",
+    "Average_10Y_Treasury_Yield": 4.5
+  },
+  {
+    "Year": 2024,
+    "Quarter": "Q4",
+    "Average_10Y_Treasury_Yield": 4.65
+  }
+]
---
+++ geog491_final/unemployment.js
@@ -0,0 +1,350 @@
+var unemployment = [
+  {
+    "observation_date": "2018-01-01",
+    "NCURN": 4.5
+  },
+  {
+    "observation_date": "2018-02-01",
+    "NCURN": 4.4
+  },
+  {
+    "observation_date": "2018-03-01",
+    "NCURN": 4.2
+  },
+  {
+    "observation_date": "2018-04-01",
+    "NCURN": 3.7
+  },
+  {
+    "observation_date": "2018-05-01",
+    "NCURN": 3.7
+  },
+  {
+    "observation_date": "2018-06-01",
+    "NCURN": 4.2
+  },
+  {
+    "observation_date": "2018-07-01",
+    "NCURN": 4.2
+  },
+  {
+    "observation_date": "2018-08-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2018-09-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2018-10-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2018-11-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2018-12-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2019-01-01",
+    "NCURN": 4.5
+  },
+  {
+    "observation_date": "2019-02-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2019-03-01",
+    "NCURN": 4
+  },
+  {
+    "observation_date": "2019-04-01",
+    "NCURN": 3.5
+  },
+  {
+    "observation_date": "2019-05-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2019-06-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2019-07-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2019-08-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2019-09-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2019-10-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2019-11-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2019-12-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2020-01-01",
+    "NCURN": 4
+  },
+  {
+    "observation_date": "2020-02-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2020-03-01",
+    "NCURN": 4.6
+  },
+  {
+    "observation_date": "2020-04-01",
+    "NCURN": 13.8
+  },
+  {
+    "observation_date": "2020-05-01",
+    "NCURN": 11.9
+  },
+  {
+    "observation_date": "2020-06-01",
+    "NCURN": 10
+  },
+  {
+    "observation_date": "2020-07-01",
+    "NCURN": 9.3
+  },
+  {
+    "observation_date": "2020-08-01",
+    "NCURN": 7
+  },
+  {
+    "observation_date": "2020-09-01",
+    "NCURN": 6.4
+  },
+  {
+    "observation_date": "2020-10-01",
+    "NCURN": 5.5
+  },
+  {
+    "observation_date": "2020-11-01",
+    "NCURN": 5.6
+  },
+  {
+    "observation_date": "2020-12-01",
+    "NCURN": 5.7
+  },
+  {
+    "observation_date": "2021-01-01",
+    "NCURN": 6.2
+  },
+  {
+    "observation_date": "2021-02-01",
+    "NCURN": 6
+  },
+  {
+    "observation_date": "2021-03-01",
+    "NCURN": 5.5
+  },
+  {
+    "observation_date": "2021-04-01",
+    "NCURN": 5.1
+  },
+  {
+    "observation_date": "2021-05-01",
+    "NCURN": 5
+  },
+  {
+    "observation_date": "2021-06-01",
+    "NCURN": 5.5
+  },
+  {
+    "observation_date": "2021-07-01",
+    "NCURN": 5.1
+  },
+  {
+    "observation_date": "2021-08-01",
+    "NCURN": 4.9
+  },
+  {
+    "observation_date": "2021-09-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2021-10-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2021-11-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2021-12-01",
+    "NCURN": 3.3
+  },
+  {
+    "observation_date": "2022-01-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2022-02-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2022-03-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2022-04-01",
+    "NCURN": 3.3
+  },
+  {
+    "observation_date": "2022-05-01",
+    "NCURN": 3.5
+  },
+  {
+    "observation_date": "2022-06-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2022-07-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2022-08-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2022-09-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2022-10-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2022-11-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2022-12-01",
+    "NCURN": 3.3
+  },
+  {
+    "observation_date": "2023-01-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2023-02-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2023-03-01",
+    "NCURN": 3.5
+  },
+  {
+    "observation_date": "2023-04-01",
+    "NCURN": 3
+  },
+  {
+    "observation_date": "2023-05-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2023-06-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2023-07-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2023-08-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2023-09-01",
+    "NCURN": 3.3
+  },
+  {
+    "observation_date": "2023-10-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2023-11-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2023-12-01",
+    "NCURN": 3.3
+  },
+  {
+    "observation_date": "2024-01-01",
+    "NCURN": 3.7
+  },
+  {
+    "observation_date": "2024-02-01",
+    "NCURN": 3.8
+  },
+  {
+    "observation_date": "2024-03-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2024-04-01",
+    "NCURN": 3.2
+  },
+  {
+    "observation_date": "2024-05-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2024-06-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2024-07-01",
+    "NCURN": 4.1
+  },
+  {
+    "observation_date": "2024-08-01",
+    "NCURN": 3.9
+  },
+  {
+    "observation_date": "2024-09-01",
+    "NCURN": 3.2
+  },
+  {
+    "observation_date": "2024-10-01",
+    "NCURN": 3.6
+  },
+  {
+    "observation_date": "2024-11-01",
+    "NCURN": 3.7
+  },
+  {
+    "observation_date": "2024-12-01",
+    "NCURN": 3.4
+  },
+  {
+    "observation_date": "2025-01-01",
+    "NCURN": 4
+  },
+  {
+    "observation_date": "2025-02-01",
+    "NCURN": 3.7
+  },
+  {
+    "observation_date": "2025-03-01",
+    "NCURN": 3.7
+  }
+]
